6d59ca9d45ff104a250ff0135f5348577faaa46c
macounj1 <48547826+macounj1@users.noreply.github.com>
2022-03-21 10:27:17 +0100

    Update rf_cv_mc.r

--- rf_cv_mc.r
+++ rf_cv_mc.r
@@ -32,8 +32,10 @@ system.time({
 })
 pdf(paste0("rf_cv_mc", nc, ".pdf")); plot(mtry_val, err/(n - n_test)); dev.off()
 
-rf.all = randomForest(lettr ~ ., train, ntree = ntree)
-pred = predict(rf.all, test)
+ntree_new = lapply(splitIndices(200, nc), length)
+rf = function(x) randomForest(lettr ~ ., train, ntree=x, norm.votes = FALSE)
+rf.out = mclapply(ntree_new, rf, mc.cores = nc)
+rf.all = do.call(combine, rf.out)
 correct = sum(pred == test$lettr)
 
 
